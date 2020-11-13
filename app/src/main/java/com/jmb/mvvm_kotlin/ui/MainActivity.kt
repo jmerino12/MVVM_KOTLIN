929c519f756cfcee3e4ecc7e7b5e3696a9e9faa4
Jonathan Meriño <jmerino1204@gmail.com>
2020-11-12 19:05:36 -0500

    implementando shimmer

--- app/src/main/java/com/jmb/mvvm_kotlin/ui/MainActivity.kt
+++ app/src/main/java/com/jmb/mvvm_kotlin/ui/MainActivity.kt
@@ -2,10 +2,12 @@ package com.jmb.mvvm_kotlin.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.facebook.shimmer.ShimmerFrameLayout
 import com.jmb.mvvm_kotlin.MainAdapter
 import com.jmb.mvvm_kotlin.viewmodel.MainViewModel
 import com.jmb.mvvm_kotlin.R
@@ -13,12 +15,14 @@ import com.jmb.mvvm_kotlin.R
 class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView:RecyclerView
     private lateinit var adapter: MainAdapter
+    private lateinit var shimmer:ShimmerFrameLayout
     private val viewModel by lazy { ViewModelProvider(this).get(MainViewModel::class.java) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         recyclerView = findViewById(R.id.recyclerView)
+        shimmer = findViewById(R.id.shimmer_view_container)
         adapter = MainAdapter(this)
 
         recyclerView.layoutManager = LinearLayoutManager(this)
@@ -29,7 +33,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun observeData(){
+        shimmer.startShimmer()
         viewModel.fetchUserData().observe(this, Observer {
+            shimmer.stopShimmer()
+            shimmer.visibility = View.GONE
             adapter.setListData(it)
             adapter.notifyDataSetChanged()
         })
